f9a362d45e80ce097178bc752e19685c41e26403
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2019-11-22 03:05:50 +0200

    add infinite loop, start to adding swipes

--- HomeWork#5/scripts/slider.js
+++ HomeWork#5/scripts/slider.js
@@ -9,6 +9,42 @@ class Slider {
         this.sliderContent = null;
         this.sliderImgs = null;
         this.buttons = null;
+
+        this._currentPos = 0;
+    }
+
+    addSwiping() {
+        this.slider.addEventListener('mousedown', this._swipeStart.bind(this));
+        this.slider.addEventListener('mouseup', this._stopSwipe.bind(this));
+
+        console.log('addSwiping')
+    }
+
+    _swipeStart(e) {
+        this._currentPos = e.clientX;
+        this.slider.addEventListener('mousemove', this._moveSlide.bind(this));
+    }
+
+    _moveSlide(e){
+        console.log(e.clientX)
+        let marginLeft = parseFloat(this.sliderContent.style.marginLeft);
+
+        if(this._currentPos > window.innerWidth/2) {
+            this.sliderContent.style.marginLeft = (marginLeft - this.slideWidth) + 'px';
+            console.log('_moveSlide')
+
+        } else {
+            this.sliderContent.style.marginLeft = (marginLeft + this.slideWidth) + 'px';
+
+        }
+
+        this._currentPos = e.clientX;
+        this.slider.removeEventListener('mousemove', this._moveSlide.bind(this));
+    }
+
+    _stopSwipe() {
+        this._currentPos = 0;
+        this.slider.removeEventListener('mousemove', this._moveSlide.bind(this));
     }
 
     createSlider(sliderSelector) {
@@ -26,12 +62,14 @@ class Slider {
         for (let i = 0; i <btns.length; i++) {
            btns[i].addEventListener('click', (evt => this.handleBtnClick(evt)));
         }
+
+        this.addSwiping();
     }
 
     sumWidthOfSlides() {
         let result = 0;
         for (let i = 0; i < this.slidesCount; i++) {
-            result += this.slideWidth + 21;
+            result += this.slideWidth;
         }
 
         return result;
@@ -50,6 +88,13 @@ class Slider {
 
         } else if (e.target.classList.contains('-next') && marginLeft < this.slideWidth && marginLeft > 0 - this.sliderContentWidth / 2) {
             this.sliderContent.style.marginLeft = (marginLeft - this.slideWidth) + 'px';
+
+        }  else if (e.target.classList.contains('-prev') && marginLeft >= 0) {
+            this.sliderContent.style.marginLeft = 0 - this.sliderContentWidth/2 + 'px';
+
+        } else if (e.target.classList.contains('-next') && marginLeft < this.slideWidth && marginLeft <= 0 - this.sliderContentWidth / 2) {
+            this.sliderContent.style.marginLeft = 0;
+
         }
 
     }
