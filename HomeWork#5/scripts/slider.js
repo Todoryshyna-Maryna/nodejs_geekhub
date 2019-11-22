762fffdee942d1b5aa1920c9ebbbff714915f6f6
Marina <marina.todorishina@gmail.com>
2019-11-22 15:47:47 +0200

    fix slider, complete swipes

--- HomeWork#5/scripts/slider.js
+++ HomeWork#5/scripts/slider.js
@@ -3,73 +3,64 @@ class Slider {
         this.slidesCount = options.slidesCount || 3;
         this.dots = options.dots || true;
         this.arrows = options.arrows || true;
-        this.slideWidth = 371;
-        this.sliderContentWidth = 371;
-        this.slider = null;
+        this._slideWidth = 354;
+        this._sliderContentWidth = 354;
+        this._slider = null;
         this.sliderContent = null;
         this.sliderImgs = null;
-        this.buttons = null;
 
         this._currentPos = 0;
+        this.stopSwipe = this._stopSwipe.bind(this);
+        this.moveSlide = this._moveSlide.bind(this);
     }
 
-    addSwiping() {
-        this.slider.addEventListener('mousedown', this._swipeStart.bind(this));
-        this.slider.addEventListener('mouseup', this._stopSwipe.bind(this));
+    _addSwiping() {
+        this.sliderContent.style.marginLeft = 0;
 
-        console.log('addSwiping')
+        this._slider.addEventListener('mousedown', this._swipeStart.bind(this));
+        this._slider.addEventListener('mouseup', this.stopSwipe);
+
+        console.log('_addSwiping')
     }
 
     _swipeStart(e) {
         this._currentPos = e.clientX;
-        this.slider.addEventListener('mousemove', this._moveSlide.bind(this));
+        this._slider.addEventListener('mousemove', this.moveSlide);
     }
 
-    _moveSlide(e){
-        console.log(e.clientX)
+    _moveSlide(e) {
         let marginLeft = parseFloat(this.sliderContent.style.marginLeft);
 
-        if(this._currentPos > window.innerWidth/2) {
-            this.sliderContent.style.marginLeft = (marginLeft - this.slideWidth) + 'px';
-            console.log('_moveSlide')
+        if (this._currentPos < e.clientX){
+            if (marginLeft < 0) {
+                this.sliderContent.style.marginLeft = (marginLeft + this._slideWidth) + 'px';
+            } else if (marginLeft >= 0) {
+                this.sliderContent.style.marginLeft = 0 - this._sliderContentWidth / 2 + 'px';
+            }
 
-        } else {
-            this.sliderContent.style.marginLeft = (marginLeft + this.slideWidth) + 'px';
+        } else  if (this._currentPos > e.clientX) {
+
+            if(marginLeft < this._slideWidth && marginLeft > 0 - this._sliderContentWidth / 2) {
+                this.sliderContent.style.marginLeft = (marginLeft - this._slideWidth) + 'px';
+            } else {
+                this.sliderContent.style.marginLeft = 0;
+            }
 
         }
 
+
         this._currentPos = e.clientX;
-        this.slider.removeEventListener('mousemove', this._moveSlide.bind(this));
+        this._slider.removeEventListener('mousemove', this.moveSlide);
     }
 
     _stopSwipe() {
-        this._currentPos = 0;
-        this.slider.removeEventListener('mousemove', this._moveSlide.bind(this));
+        this._slider.removeEventListener('mousemove', this.moveSlide);
     }
 
-    createSlider(sliderSelector) {
-        this.slider = document.querySelector(sliderSelector);
-        this.sliderContent = document.querySelector(sliderSelector + ' .slider-content');
-        this.sliderImgs = document.querySelectorAll(sliderSelector + ' .slide-image');
-        this.slidesCount = this.sliderImgs.length;
-
-        this.sliderContentWidth = this.sumWidthOfSlides();
-        this.sliderContent.style.width = this.sliderContentWidth + 'px';
-
-        let btns =  this.sliderImgs = document.querySelectorAll(sliderSelector + ' .btn');
-
-
-        for (let i = 0; i <btns.length; i++) {
-           btns[i].addEventListener('click', (evt => this.handleBtnClick(evt)));
-        }
-
-        this.addSwiping();
-    }
-
-    sumWidthOfSlides() {
+    _sumWidthOfSlides() {
         let result = 0;
         for (let i = 0; i < this.slidesCount; i++) {
-            result += this.slideWidth;
+            result += this._slideWidth;
         }
 
         return result;
@@ -84,19 +75,38 @@ class Slider {
         }
 
         if (e.target.classList.contains('-prev') && marginLeft < 0) {
-            this.sliderContent.style.marginLeft = (marginLeft + this.slideWidth) + 'px';
+            this.sliderContent.style.marginLeft = (marginLeft + this._slideWidth) + 'px';
 
-        } else if (e.target.classList.contains('-next') && marginLeft < this.slideWidth && marginLeft > 0 - this.sliderContentWidth / 2) {
-            this.sliderContent.style.marginLeft = (marginLeft - this.slideWidth) + 'px';
+        } else if (e.target.classList.contains('-next') && marginLeft < this._slideWidth && marginLeft > 0 - this._sliderContentWidth / 2) {
+            this.sliderContent.style.marginLeft = (marginLeft - this._slideWidth) + 'px';
 
-        }  else if (e.target.classList.contains('-prev') && marginLeft >= 0) {
-            this.sliderContent.style.marginLeft = 0 - this.sliderContentWidth/2 + 'px';
+        } else if (e.target.classList.contains('-prev') && marginLeft >= 0) {
+            this.sliderContent.style.marginLeft = 0 - this._sliderContentWidth / 2 + 'px';
 
-        } else if (e.target.classList.contains('-next') && marginLeft < this.slideWidth && marginLeft <= 0 - this.sliderContentWidth / 2) {
+        } else if (e.target.classList.contains('-next') && marginLeft < this._slideWidth && marginLeft <= 0 - this._sliderContentWidth / 2) {
             this.sliderContent.style.marginLeft = 0;
 
         }
 
+    }
+
+    createSlider(sliderSelector) {
+        this._slider = document.querySelector(sliderSelector);
+        this.sliderContent = document.querySelector(sliderSelector + ' .slider-content');
+        this.sliderImgs = document.querySelectorAll(sliderSelector + ' .slide-image');
+        this.slidesCount = this.sliderImgs.length;
+
+        this._sliderContentWidth = this._sumWidthOfSlides();
+        this.sliderContent.style.width = this._sliderContentWidth + 'px';
+
+        let btns = this.sliderImgs = document.querySelectorAll(sliderSelector + ' .btn');
+
+
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener('click', (evt => this.handleBtnClick(evt)));
+        }
+
+        this._addSwiping();
     }
 
 }
